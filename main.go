9ecc76e47d7d274f7f20244ab79c4d99ebea71c6
RayLeeRun <fpboy0070314@gmail.com>
2020-01-09 15:11:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func notifyHandler(w http.ResponseWriter, r *http.Request) {
 
 	res := newTokenResponse(byt)
 	fmt.Println("result:", res)
-	token = res.AccessToken
+	token = "MJdfRUGaljIM1jc18IN2vMBxCSkbbNtNhGHxYnkJkAb"
 	w.Write(byt)
 }
 
